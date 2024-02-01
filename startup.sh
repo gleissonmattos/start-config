d88625c5e92e3c7fd5c39f2162fdec5562a1f95e
Gleisson Matos <contato.mattos@outlook.com>
2024-01-31 21:01:38 -0300

    feat: change default terminal to zsh on finish script

--- startup.sh
+++ startup.sh
@@ -54,7 +54,6 @@ cat ~/.ssh/id_rsa.pub
 echo 'INSTALL zsh'
 sudo apt-get install zsh -y
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-chsh -s /bin/zsh
 
 echo 'INSTALL autosuggestions' 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
@@ -196,3 +195,5 @@ echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sou
 sudo apt-get update && sudo apt-get install spotify-client
 
 echo 'finished'
+
+chsh -s /bin/zsh
