304a1d4530ecb599bc0369c98813226c507c7518
Gleisson Mattos <contato.mattos@outlook.com>
2024-02-16 09:40:24 -0300

    feat always install the new version of docker-compose

--- startup.sh
+++ startup.sh
@@ -155,7 +155,7 @@ docker --version
 chmod 777 /var/run/docker.sock
 
 echo 'INSTALL docker-compose' 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
